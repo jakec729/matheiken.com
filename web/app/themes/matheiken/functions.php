f48352bf14c814c0deadc9c2eb055a1eb0a98f5e
Jake Cooper <jakec729@mac.com>
2016-05-23 08:57:11 -0400

    Reorganizing files

--- web/app/themes/matheiken/functions.php
+++ web/app/themes/matheiken/functions.php
@@ -1,69 +1,38 @@
 <?php
 /**
  * Matheiken Theme functions and definitions.
- *
- * @link https://developer.wordpress.org/themes/basics/theme-functions/
- *
- * @package Matheiken_Theme
  */
 
-if ( ! function_exists( 'matheiken_setup' ) ) :
-/**
- * Sets up theme defaults and registers support for various WordPress features.
- *
- * Note that this function is hooked into the after_setup_theme hook, which
- * runs before the init hook. The init hook is too late for some features, such
- * as indicating support for post thumbnails.
- */
-function matheiken_setup() {
-	/*
-	 * Make theme available for translation.
-	 * Translations can be filed in the /languages/ directory.
-	 * If you're building a theme based on Matheiken Theme, use a find and replace
-	 * to change 'matheiken' to the name of your theme in all the template files.
-	 */
-	load_theme_textdomain( 'matheiken', get_template_directory() . '/languages' );
-
-	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
+if ( ! function_exists( 'matheiken_setup' ) ) {
 
 	/*
-	 * Let WordPress manage the document title.
-	 * By adding theme support, we declare that this theme does not use a
-	 * hard-coded <title> tag in the document head, and expect WordPress to
-	 * provide it for us.
+	 * Theme Setup
 	 */
-	add_theme_support( 'title-tag' );
+	function matheiken_setup() {
+		add_theme_support( 'automatic-feed-links' );
+		add_theme_support( 'title-tag' );
+		add_theme_support( 'post-thumbnails' );
 
-	/*
-	 * Enable support for Post Thumbnails on posts and pages.
-	 *
-	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-	 */
-	add_theme_support( 'post-thumbnails' );
+		register_nav_menus( array(
+			'primary' => esc_html__( 'Primary', 'matheiken' ),
+			'social' => esc_html__( 'Social', 'matheiken' ),
+		) );
 
-	// This theme uses wp_nav_menu() in one location.
-	register_nav_menus( array(
-		'primary' => esc_html__( 'Primary', 'matheiken' ),
-		'social' => esc_html__( 'Social', 'matheiken' ),
-	) );
-
-	/*
-	 * Switch default core markup for search form, comment form, and comments
-	 * to output valid HTML5.
-	 */
-	add_theme_support( 'html5', array(
-		'search-form',
-		'comment-form',
-		'comment-list',
-		'gallery',
-		'caption',
-	) );
-
+		add_theme_support( 'html5', array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+		) );
+	}
 }
-endif;
 add_action( 'after_setup_theme', 'matheiken_setup' );
 
+
+/*
+ * Theme Actions and Filters -- Adjustments
+ */
 add_action( 'after_setup_theme', function() {
 	$GLOBALS['content_width'] = apply_filters( 'matheiken_content_width', 640 );
 }, 0 );
@@ -80,10 +49,9 @@ add_filter( 'get_the_archive_title', function ( $title ) {
 	return (is_category()) ? single_cat_title("", false) : $title;
 });
 
+
 /**
  * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
 function matheiken_widgets_init() {
 	register_sidebar( array(
@@ -111,14 +79,9 @@ add_action( 'widgets_init', 'matheiken_widgets_init' );
 /**
  * Enqueue scripts and styles.
  */
-function matheiken_scripts() {
+add_action( 'wp_enqueue_scripts', function() {
 	wp_enqueue_style( 'matheiken-style', get_stylesheet_uri() );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
-}
-add_action( 'wp_enqueue_scripts', 'matheiken_scripts' );
+});
 
 /**
  * Custom template tags for this theme.
--- web/app/themes/matheiken/inc/customizer.php
+++ web/app/themes/matheiken/inc/customizer.php
@@ -20,6 +20,6 @@ add_action( 'customize_register', 'matheiken_customize_register' );
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function matheiken_customize_preview_js() {
-	wp_enqueue_script( 'matheiken_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+	wp_enqueue_script( 'matheiken_customizer', get_template_directory_uri() . '/assets/js/customizer.js', array( 'customize-preview' ), '20151215', true );
 }
 add_action( 'customize_preview_init', 'matheiken_customize_preview_js' );
